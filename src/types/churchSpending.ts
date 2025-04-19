deca178b26dff66611635aa4af3a48af397ce503
Susilawati <susilawatibudiutami@gmail.com>
2025-04-19 19:10:15 +0700

    feat: add logic for all church

---
+++ src/types/churchSpending.ts
@@ -0,0 +1,29 @@
+export interface ChurchSpendingCreateParams {
+  id: bigint;
+  detail: string;
+  funds: bigint;
+  date: Date;
+  spendingTypeId: bigint;
+  spendingTypeName: string;
+  description: string;
+  code: string;
+  bill: string | null;
+  billNumber: string | null;
+}
+
+export interface ChurchSpendingUpdateParams {
+  id: bigint;
+  detail: string;
+  funds: bigint;
+  date: Date;
+  spendingTypeId: bigint;
+  spendingTypeName: string;
+  description: string;
+  code: string;
+  bill: string | null;
+  billNumber: string | null;
+  createdBy: string;
+  createdAt: Date;
+  updatedAt: Date;
+  updatedBy: bigint;
+}
